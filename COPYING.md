19071d9a29e313a2d1fcb19bec964bb7d70987b5
Justin Reardon <justin.reardon@gmail.com>
2020-12-12 12:29:48 -0500

    phaser template

---
+++ COPYING.md
@@ -0,0 +1,18 @@
+# Code
+
+Portions of this software are copyright 2020 Justin Reardon.
+
+Portions of this software are copyright 2020 James Skemp, and licensed 
+under the MIT license. See the file MIT_LICENSE.
+
+This software uses the Phaser 3 library, Copyright 2020 Richard Davey, 
+Photon Storm Ltd which is licensed under the MIT license. See the file MIT_LICENSE.
+
+# Assets
+
+*Insert description here.*
+
+## Fonts
+
+*Insert description here.*
+
---
+++ src/ts/Scenes/Boot.ts
@@ -0,0 +1,23 @@
+/* 
+ * Portions copyright 2020, James Kemp.
+*/
+
+import Preloader from "./Preloader";
+import Utilities from "../Utilities";
+
+export default class Boot extends Phaser.Scene {
+  /**
+   * Unique name of the scene.
+   */
+  public static Name = "Boot";
+
+  public preload(): void {
+    // Preload as needed.
+  }
+
+  public create(): void {
+    Utilities.LogSceneMethodEntry("Boot", "create");
+
+    this.scene.start(Preloader.Name);
+  }
+}
